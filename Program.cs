b8d41e4824e1d01dc81961aef7572eaf958f0fa3
rinfz <mr@b5.re>
2023-11-18 12:18:33 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO.Compression;
+using System.Text;
+
+var acceptedCookie = false;
+
+static Func<IWebDriver, IWebElement?> ElementIsClickable(By locator) {
+  return driver => {
+    var element = driver.FindElement(locator);
+    return (element != null && element.Displayed && element.Enabled) ? element : null;
+  };
+}
+
+void ProcessFile(IWebDriver driver, string url, string dlPath, int currentCount) {
+  driver.Navigate().GoToUrl(url);
+  var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+  if (!acceptedCookie) {
+    var iframe = wait.Until(d => d.FindElement(By.CssSelector("iframe[id^=sp_message_iframe]")));
+    driver.SwitchTo().Frame(iframe);
+    var accept = wait.Until(d => d.FindElement(By.XPath("//button[.='Accept']")));
+    accept.Click();
+    driver.SwitchTo().DefaultContent();
+    Thread.Sleep(500);
+    acceptedCookie = true;
+  }
+
+  var dl = wait.Until(ElementIsClickable(By.ClassName("download-cta")))!;
+  dl.Click();
+
+  var dlWait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+  dlWait.Until(_ => Directory.GetFiles(dlPath).Count() > currentCount);
+}
+
+var input = File.ReadAllLines("addons.txt");
+
+var downloadsDir = input.First();
+var addonsDir = input.Skip(1).First();
+
+var urls = input.Skip(2);
+var downloadFilesCount = urls.Count();
+
+var filesBefore = Directory.GetFiles(downloadsDir);
+var currentCount = filesBefore.Count();
+
+var opts = new ChromeOptions();
+opts.AddArguments("--start-fullscreen");
+var driver = new ChromeDriver(opts);
+
+foreach (var url in urls) {
+  ProcessFile(driver, url, downloadsDir, currentCount);
+  currentCount += 1;
+}
+
+var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+var newFiles = wait.Until(_ => {
+  var currentFiles = Directory.GetFiles(downloadsDir);
+  var filesDiff = currentFiles.Where(f => !filesBefore.Contains(f));
+  if (filesDiff.All(f => f.EndsWith(".zip"))) {
+    return filesDiff;
+  }
+  return null;
+})!;
+
+driver.Quit();
+
+// unzip new files
+
+Thread.Sleep(500);
+
+foreach (var zipFile in newFiles) {
+  var archive = ZipFile.OpenRead(zipFile);
+  var hasToc = archive.Entries.Any(f => f.Name.EndsWith(".toc"));
+  archive.Dispose();
+  if (hasToc) {
+    ZipFile.ExtractToDirectory(zipFile, addonsDir, Encoding.Default, true);
+    try {
+      File.Delete(zipFile);
+    } catch (IOException) {
+      Console.WriteLine("Failed to delete file, please delete manually ({0})", zipFile);
+    }
+  } else {
+    Console.WriteLine("Unable to process addon, no TOC file found ({0})", zipFile);
+  }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# Wow DL
+
+Automate downloading wow addons without having to use the usual adware apps.
+
+## Requirements
+
+You will need to download a chromedriver exe whose version matches your installed
+version of chrome. Chances are you can just download the stable version.
+
+Assuming you are on x64 windows, copy the url from the chromedriver win64 row
+to download it. Copy chromedriver.exe from the .zip file and place it alongside
+the wowdl binary.
+
+![How to download chromedriver](chromedriver.png)
+
+## Configure
+
+Place a file called `addons.txt` alongside the wowdl binary. Here is an example:
+
+```
+C:\Users\user\Downloads
+C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns
+https://www.curseforge.com/wow/addons/mythic-dungeon-tools
+https://www.curseforge.com/wow/addons/details
+https://www.curseforge.com/wow/addons/big-wigs
+https://www.curseforge.com/wow/addons/little-wigs
+https://www.curseforge.com/wow/addons/weakauras-2
+https://www.curseforge.com/wow/addons/pettracker
+https://www.curseforge.com/wow/addons/mythic-dungeon-tools
+https://www.curseforge.com/wow/addons/battle_pet_breedid
+```
+
+The first line is the directory where your browser downloads files.
+
+The second is the location of your wow addon directory.
+
+The rest of the lines are the curseforge urls of the addons you want.
+
+Your final directory setup should look like the following:
+
+![Final setup](dir-setup.png)
